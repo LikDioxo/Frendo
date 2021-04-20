b8bf989029ab3a2cf980e82ddc1f2a2fe257386b
LikDioxo <likdioxo@gmail.com>
2021-04-20 18:32:12 +0300

    add new symfony package to avoid CORS errors

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import loup from "../assets/images/loup.jpg";
 
+
 function SearchBox({onFilterView})
 {
 
     return (<div className="search-box">
             <div className="search">
                 <input type="text" size="40" className="search"/>
-                <button className="search-button" ><img src={loup} alt=""/></button>
+                <button className="search-button">
+                    <img src={loup} alt=""/>
+                </button>
             </div>
             <button className="button" onClick={onFilterView}>Фильтр ингредиентов</button>
         </div>
