7bd541d3d578b404cefcb05fc48c7cfc0eca4f93
LikDioxo <likdioxo@gmail.com>
2021-04-20 17:22:29 +0300

    add get pizzeria's pizzas service

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.baseURL = '127.0.0.1:80';
 
 
-function getPizzeriaAvailablePizzas(pizzeria_id) {
+export function getPizzeriaAvailablePizzas(pizzeria_id) {
     return axios(
         {
             method: 'get',
