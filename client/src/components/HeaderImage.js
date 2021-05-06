34eb6146ea7e7c129ced86d1480b378d51358385
blakfar <43219309+blakfar@users.noreply.github.com>
2021-05-06 17:19:15 +0300

    add link to home page from header image

--- client/src/components/HeaderImage.js
+++ client/src/components/HeaderImage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from "../assets/images/logo.png";
 import "../assets/css/header_image.css"
+import {Link} from "react-router-dom";
 
 
 function HeaderImage()
 {
     return (
         <div className="header-image">
-            <img src={logo} alt="Логотип"/>
+            <Link to={"/"}>
+                <img src={logo} alt="Логотип"/>
+            </Link>
         </div>
     );
 }
