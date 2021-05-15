56face322d56678821e920e1884100accd6cd386
LikDioxo <likdioxo@gmail.com>
2021-05-16 01:44:27 +0300

    Add operator order details pizza list components and styles for it

---
+++ client/src/components/OperatorOrderDetailsPizzaList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import OperatorOrderDetailPizzaItem from "./OperatorOrderDetailPizzaItem";
+import "../assets/css/operator_order_details_pizza_list.css";
+
+
+function OperatorOrderDetailsPizzaList()
+{
+    return (
+        <div className="operator-order-details-pizza-list">
+            <OperatorOrderDetailPizzaItem/>
+        </div>
+    )
+}
+
+
+export default OperatorOrderDetailsPizzaList
