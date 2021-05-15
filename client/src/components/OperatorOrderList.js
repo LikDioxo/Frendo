a5b903d24a095c667d9490db7783dfd55039db22
LikDioxo <likdioxo@gmail.com>
2021-05-15 22:37:01 +0300

    Add operator order list component

---
+++ client/src/components/OperatorOrderList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import OperatorOrderItem from "./OperatorOrderItem";
+import "../assets/css/operator_order_list.css";
+
+
+function OperatorOrderList()
+{
+    return (
+        <div className="operator-order-list">
+            <OperatorOrderItem/>
+        </div>
+    )
+}
+
+
+export default OperatorOrderList
